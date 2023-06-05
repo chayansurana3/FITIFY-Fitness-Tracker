5dc1dfbeaf7fa67b092fed941908aa864aad3788
Chayan <chayan.surana3@gmail.com>
2023-06-05 11:16:49 +0530

    new updates

--- netlify/functions/profile.js
+++ netlify/functions/profile.js
@@ -2,33 +2,46 @@ const mongoose = require('mongoose');
 const username = process.env.MONGODB_USERNAME;
 const password = process.env.MONGODB_PASSWORD;
 
-mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.e6wowbu.mongodb.net/FITIFY_PROFILES?retryWrites=true&w=majority`);
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.e6wowbu.mongodb.net/FITIFY_PROFILES?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 const Profile = require('./profileModel');
 
-exports.handler = async function (event, context, callback) {
+exports.handler = async function (event, context) {
+  const timeout = 20000; // Timeout value in milliseconds (e.g., 10 seconds)
+  
   return new Promise(async (resolve, reject) => {
-    const unique_code = event.queryStringParameters.uniqueCode;
-    console.log(unique_code);
-
+    const timeoutHandler = setTimeout(() => {
+      reject(new Error('Function execution timed out'));
+    }, timeout);
+    
     try {
+      const unique_code = event.queryStringParameters.uniqueCode;
+      console.log(unique_code);
+
       const user_profile = await Profile.findOne({ uniqueCode: unique_code });
       if (!user_profile) {
-        return callback(null, {
+        clearTimeout(timeoutHandler); 
+        return {
           statusCode: 404,
           body: JSON.stringify({ error: 'Profile not found' }),
-        });
+        };
       }
 
-      return callback(null, {
+      clearTimeout(timeoutHandler); 
+      return {
         statusCode: 200,
         body: JSON.stringify(user_profile),
-      });
+      };
     } catch (error) {
+      clearTimeout(timeoutHandler);
       console.log(error);
-      return callback(null, {
+      return {
         statusCode: 500,
         body: JSON.stringify({ error: 'Server error' }),
-      });
+      };
     }
   });
 };
