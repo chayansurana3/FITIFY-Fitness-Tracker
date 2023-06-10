9e5338dec64cc223cf4f184ff22c85023e7231b1
Chayan <chayan.surana3@gmail.com>
2023-06-10 13:35:41 +0530

    added delete data functionality to website

---
+++ netlify/functions/delete.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+exports.handler = async function(event, context, callback) {
+  try {
+    const username = process.env.MONGODB_USERNAME;
+    const password = process.env.MONGODB_PASSWORD;
+    await mongoose.connect(
+      `mongodb+srv://${username}:${password}@cluster0.e6wowbu.mongodb.net/FITIFY_PROFILES?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log('Connected to MongoDB');
+    const Profile = require('./profileModel');
+
+    const unique_code = event.queryStringParameters.uniqueCode;
+    console.log(unique_code);
+
+    const deleteResult = await Profile.deleteOne({ uniqueCode: unique_code });
+    if (deleteResult.deletedCount === 0) {
+      return callback(null, {
+        statusCode: 404,
+        body: JSON.stringify({ error: 'Profile not found' }),
+      });
+    }
+
+    await mongoose.disconnect();
+    console.log('Disconnected from MongoDB');
+
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Profile deleted successfully' }),
+    });
+  } catch (error) {
+    console.log(error);
+    return callback(null, {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Server error' }),
+    });
+  }
+};
